bf7ae7e3e87313665fa72753b54d639819c2b5bd
Marco Monroy <44242067+ronmoy007@users.noreply.github.com>
2020-03-04 00:21:44 -0600

    Update 60_send_to_s3.py

--- Scripts/60_send_to_s3.py
+++ Scripts/60_send_to_s3.py
@@ -2,8 +2,8 @@ import boto3
 
 s3=boto3.client(
         's3',
-	    aws_access_key_id='AKIA4VZTGLQQYT2OZGMJ',
-        aws_secret_access_key='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',
+	    aws_access_key_id='USUARIO',
+        aws_secret_access_key='CONTRASEÑA',
         region_name='us-west-2', #Oregon
         use_ssl=False
     )
