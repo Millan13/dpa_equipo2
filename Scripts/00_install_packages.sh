72378c8c442bfac32828cd8514fd606d59d7e9e1
Marco Monroy <44242067+ronmoy007@users.noreply.github.com>
2020-03-30 17:28:16 -0600

    Add files via upload

--- Scripts/00_install_packages.sh
+++ Scripts/00_install_packages.sh
@@ -3,6 +3,7 @@ wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 chromedriver --version
+cp /usr/bin/chromedriver .
 
 curl https://intoli.com/install-google-chrome.sh | bash
 sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
@@ -22,3 +23,9 @@ python3 get-pip.py --user
 pip3 --version
 
 pip3 install selenium --user
+pip3 install luigi
+pip3 install boto3
+pip3 install glob
+
+sudo yum update
+sudo yum install postgresql postgresql-contrib
