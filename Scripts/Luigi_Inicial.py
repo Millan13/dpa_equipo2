89f5bc7075ceba864f3b6552d652c325b0c0f143
EC2 Default User <ec2-user@ip-172-31-30-79.us-west-2.compute.internal>
2020-04-04 15:42:04 +0000

    Cambios para generar el output solo si corre bien la tarea, cambio de numeracion

---
+++ Scripts/Luigi_Inicial.py
@@ -0,0 +1,84 @@
+# Librerias de python
+import luigi
+import os
+import time
+
+# Librerias de nosotros
+from Rita import Auxiliar
+import Luigi_Tasks as lt
+
+class Tarea_10(luigi.Task):
+
+    def run(self):
+        # Si puede crear los directorios bien, generamos el archivo output
+        if lt.CrearDirectoriosEC2() == 0:
+            os.system('echo OK > Tarea_10')
+
+    def output(self):
+        return luigi.LocalTarget('Tarea_10')
+
+class Tarea_20(luigi.Task):
+
+    def requires(self):
+        return Tarea_10()
+
+    def run(self):
+
+        if lt.CrearDirectoriosS3() == 0:
+            os.system('echo OK > Tarea_20')
+
+    def output(self):
+        str_DirTrabajo=Auxiliar().ObtenerDirectorioTrabajo()
+        return luigi.LocalTarget('Tarea_20')
+
+class Tarea_30(luigi.Task):
+
+    def requires(self):
+        return Tarea_20()
+
+    def run(self):
+        if lt.CrearSchemasRDS() == 0:
+            os.system('echo OK > Tarea_30')
+
+    def output(self):
+        return luigi.LocalTarget('Tarea_30')
+
+class Tarea_40(luigi.Task):
+
+    def requires(self):
+        return Tarea_30()
+
+    def run(self):
+        if lt.CrearTablasLinajeRDS() == 0:
+            os.system('echo OK > Tarea_40')
+
+    def output(self):
+        return luigi.LocalTarget('Tarea_40')
+
+class Tarea_50(luigi.Task):
+
+    def requires(self):
+        return Tarea_40()
+
+    def run(self):
+        if lt.WebScrapingInicial() == 0:
+            os.system('echo OK > Tarea_50')
+
+    def output(self):
+        return luigi.LocalTarget('Tarea_50')
+
+class Tarea_60(luigi.Task):
+
+    def requires(self):
+        return Tarea_50()
+
+    def run(self):
+        if lt.EnviarMetadataLinajeRDS() == 0:
+            os.system('echo OK > Tarea_60')
+
+    def output(self):
+        return luigi.LocalTarget('Tarea_60')
+
+
+if __name__ == '__main__':
+    luigi.run()
