c841692a26ff0ad63bb1b0332aad614c72cca443
Marco <ronmoy007@hotmail.com>
2020-04-10 14:24:28 -0500

    Ajuste a las tablas de linaje

--- Scripts/sql/create_linaje_tables.sql
+++ Scripts/sql/create_linaje_tables.sql
@@ -6,13 +6,11 @@
 /**************************** Titulo ejecuciones ****************************/
 drop table if exists linaje.ejecuciones;
 create table linaje.ejecuciones (
-  "id_ejec" VARCHAR(20),
-  "id_archivo" VARCHAR(40),
+  "id_ejec" NUMERIC,
   "usuario_ejec" VARCHAR(20),
   "instancia_ejec" VARCHAR(20),
   "fecha_hora_ejec" TIMESTAMP,
   "bucket_s3" VARCHAR(40),
-  "ruta_almac_s3" VARCHAR(100),
   "tag_script" VARCHAR(20),
   "tipo_ejec" VARCHAR(5),
   "url_webscrapping" VARCHAR(100),
@@ -24,12 +22,14 @@ comment on table linaje.ejecuciones is 'describe los datos principales de las ej
 /**************************** Titulo archivos ****************************/
 drop table if exists linaje.archivos;
 create table linaje.archivos (
+  "id_ejec" NUMERIC,
   "id_archivo" VARCHAR(40),
   "num_registros" VARCHAR(20),
   "num_columnas" VARCHAR(10),
   "tamanio_archivo" FLOAT,
   "anio" VARCHAR(5),
-  "mes" VARCHAR(10)
+  "mes" VARCHAR(10),
+  "ruta_almac_s3" VARCHAR(100)
 );
 comment on table linaje.archivos is 'describe caracteristicas especificas de archivos';
 
